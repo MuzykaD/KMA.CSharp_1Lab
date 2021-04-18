0fd68a5c3e63ef1aca974879adaaf6bc810b647e
Pershuta001 <48165099+Pershuta001@users.noreply.github.com>
2021-04-18 12:01:26 +0300

    [back] add Serialization Manager

--- WpfApp1/Tools/FileFolderHelper.cs
+++ WpfApp1/Tools/FileFolderHelper.cs
@@ -1,12 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace WpfApp1.Tools
 {
-    class FileFolderHelper
+    internal static class FileFolderHelper
     {
+        private static readonly string AppDataPath =
+            Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+
+        internal static readonly string AppFolderPath =
+            Path.Combine(AppDataPath, "CSharpProj");
+
+        internal static readonly string StorageFilePath =
+            Path.Combine(AppFolderPath, "Storage.cskma");
+
+        internal static bool CreateFolderAndCheckFileExistance(this FileInfo file)
+        {
+            if (!file.Directory.Exists)
+            {
+                file.Directory.Create();
+            }
+            return file.Exists;
+        }
+
+        internal static bool CreateFolderAndCheckFileExistance(string filePath)
+        {
+            var file = new FileInfo(filePath);
+            return file.CreateFolderAndCheckFileExistance();
+        }
     }
 }
